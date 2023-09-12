0780191ec2feb19bffec3fc10747e7f6413bc9f2
Kauan <kauan.barbosa@aluno.ifsp.edu.br>
2023-09-12 08:24:11 -0300

    Inclui flash messages.

--- polls/views.py
+++ polls/views.py
@@ -40,27 +40,31 @@ class QuestionCreateView(CreateView):
     template_name = 'polls/question_form.html'
     fields = ('question_text', 'pub_date', )
     success_url = reverse_lazy('polls_list')
-
-def get_context_data(self, **kwargs):
-                context = super(QuestionCreateView, self).get_context_data(**kwargs)
-                context['form_title'] = 'Criando uma pergunta'
-                return context
+    def get_context_data(self, **kwargs):
+        context = super(QuestionCreateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Criando uma pergunta'
+        return context
 
 class QuestionUpdateView(UpdateView):
     model = Question
     template_name = 'polls/question_form.html'
     fields = ('question_text', 'pub_date', )
     success_url = reverse_lazy('polls_list')
-
-def get_context_data(self, **kwargs):
-    context = super(QuestionUpdateView, self).get_context_data(**kwargs)
-    context['form_title'] = 'Editando a pergunta'
-    return context
+    def get_context_data(self, **kwargs):
+        context = super(QuestionUpdateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Editando a pergunta'
+        return context
 
 class QuestionDeleteView(DeleteView):
     model = Question
     template_name = 'polls/question_confirm_delete_form.html'
     success_url = reverse_lazy('polls_list')
+    success_message = 'Pergunta excluída com sucesso.'
+    def form_valid(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(QuestionDeleteView, self).form_valid(request, *args, **kwargs)
+
+
 
 from django.views.generic import DetailView, ListView, TemplateView
 
@@ -78,6 +82,8 @@ class QuestionListView(ListView):
 
 class SobreTemplateView(TemplateView):
     template_name = 'polls/sobre.html'
+
+from django.contrib import messages
 
 
 
